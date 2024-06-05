c67f4076f9a3ae815a9bd4ebbe13cee82bc0b15d
AlbaraaAlkhateeb <alkhateebalbaraa69@gmail.com>
2024-06-05 16:30:38 +0300

    encryption code
    Take a look at the encryption code

---
+++ py2.py
@@ -0,0 +1,34 @@
+# import string
+# def encrypt(message, shift):
+#     alphabet = string.ascii_lowercase
+
+#     decrypted_message = ""
+
+#     for letter in message:
+#         if letter.lower() in alphabet:
+#             original_position = alphabet.index(letter.lower())
+#             new_position = (original_position - shift) % 26
+#             decrypted_letter = alphabet[new_position]
+#             if letter.isupper():
+#                 decrypted_letter = decrypted_letter.upper()
+
+#             decrypted_message += decrypted_letter
+#         else:
+#             decrypted_message += letter
+#     print(f"\n\nHere is the original message\n*****\n{decrypted_message}\n\n*****")
+
+# user_message = input("Enter a message: ")
+# shift_number = int(input("Enter a shift number: "))
+
+
+# encrypt(message=user_message,shift=shift_number)
+
+book = {
+    "title": "Red Queen",
+    "author": "Victoria Aveyard",
+    "year": 2015,
+    "pages": 383,
+    "is_many": True,
+    "rating": 4.1,
+}
+print(book["author"])
